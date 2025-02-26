217c1e501b8b644f1aa3308b9380ae3e3139309d
samuelsen <santossamuelsouza@gmail.com>
2025-02-26 00:32:42 -0300

    Projeto conta banco finalizado

--- desafio-dados-bancarios/src/ContaTerminal.java
+++ desafio-dados-bancarios/src/ContaTerminal.java
@@ -1,5 +1,25 @@
+import java.util.Locale; 
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner (System.in).useLocale(Locale.US);
+
+        int numero = 1021;
+        String agencia = "067-8";
+        String nomeCliente = "MARIO ANDRADE"; 
+        double saldo = 237.48;
+        //Exibir mensagens para o nosso usuário 
+        System.out.println("Digite sua agencia: ");
+        String agenciaCliente = scanner.next();
+
+        if (agenciaCliente.equals(agencia)){
+            System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo R$" + saldo + " já está disponível para saque." );
+        } 
+        else { 
+            System.out.println("Agencia digitada não existe! ");
+        }
+        //Obter pela scanner os valores digitados no terminal 
+        scanner.close();
     }
 }
